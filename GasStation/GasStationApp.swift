d5c8ec50bd79ed2494ebc46f96216be54fee57f7
Vivian Phung <vivianphung@outlook.com>
2021-12-16 18:25:37 -0500

    Initial Commit

---
+++ GasStation/GasStationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GasStationApp.swift
+//  GasStation
+//
+//  Created by Vivian Phung on 12/16/21.
+//
+
+import SwiftUI
+
+@main
+struct GasStationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
